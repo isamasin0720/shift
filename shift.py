dafdd440e6d0c320a4805632ece0e2b76773fae0
Yuma <yuma.nozaki@konicaminolta.com>
2021-02-06 14:41:31 +0900

    0206eighth commit

--- shift.py
+++ shift.py
@@ -212,4 +212,5 @@ def insert():
     return redirect(url_for('add'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
