52a872ad7fd7670cbdddc71e5c845acff1d7c57a
Yuma <yuma.nozaki@konicaminolta.com>
2021-02-06 14:46:17 +0900

    0206nineth commit

--- shift.py
+++ shift.py
@@ -1,5 +1,6 @@
 #coding: UTF-8
 from flask import Flask, render_template, request, redirect, url_for
+import os
 import sqlite3
 
 app = Flask(__name__)
@@ -212,5 +213,4 @@ def insert():
     return redirect(url_for('add'))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
